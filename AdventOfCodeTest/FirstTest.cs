3405e458b3c8948f481fa58beb248dbb0efee829
Jonathan Daugaard <jonathan.daugaard@carmenta.com>
2020-11-28 18:18:05 +0100

    Init repo

---
+++ AdventOfCodeTest/FirstTest.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace AdventOfCodeTest
+{
+    public class FirstTest
+    {
+        [Fact]
+        public void TestMethod()
+        {
+            Assert.True(true);
+        }
+    }
+}
