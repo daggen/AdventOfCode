31df0c2df9b6786f2a3203dec87df8910a35fc02
Jonathan Daugaard <jonathan.daugaard@carmenta.com>
2020-12-06 13:28:03 +0100

    Day6

---
+++ AdventOfCodeLib/Utils/Utils.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCodeLib.Utils
+{
+    public static class Utils
+    {
+        public static IEnumerable<T> Intersect<T>(this IEnumerable<IEnumerable<T>> source)
+        {
+            return source.Aggregate((current, next) => current.Intersect(next));
+        }
+
+        public static IEnumerable<IEnumerable<T>> Split<T>(this IEnumerable<T> source, T separator)
+        {
+            var chunk = new List<T>();
+            foreach (var item in source)
+            {
+                if (Equals(item, separator))
+                {
+                    yield return chunk;
+                    chunk = new List<T>();
+                }
+                else
+                {
+                    chunk.Add(item);
+                }
+            }
+            yield return chunk;
+        }
+    }
+}
---
+++ AdventOfCodeTest/Day6.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdventOfCodeLib.Utils;
+using Xunit;
+
+namespace AdventOfCodeTest
+{
+    public class Day6
+    {
+        private const string FileNameInput = "../../Input/inputDay6.txt";
+        public IEnumerable<string> Input => File.ReadAllLines(FileNameInput);
+
+        [Fact]
+        public void TestCountNUmberOfYes()
+        {
+            var totalCount = Input.Split("")
+                                  .Select(ls => string.Join("", ls))
+                                  .Select(answer => answer.Distinct())
+                                  .Select(nbrOfAnswers => nbrOfAnswers.Count())
+                                  .Sum();
+            Assert.Equal(6170, totalCount);
+        }
+
+        [Fact]
+        public void TestReadFile()
+        {
+            var count = Input.Split("").Select(l => l.Intersect().Count()).Sum();
+            Assert.Equal(2947, count);
+        }
+    }
+}
