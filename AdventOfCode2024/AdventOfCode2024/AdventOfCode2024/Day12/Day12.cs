aef8aab548b67de7e93a6b3a77c63725becd533c
Jonathan Daugaard <jonathan.daugaard@cabhealthcase.se>
2024-12-12 16:57:06 +0100

    day 12 part 1

--- AdventOfCode2024/AdventOfCode2024/AdventOfCode2024/Day12/Day12.cs
+++ AdventOfCode2024/AdventOfCode2024/AdventOfCode2024/Day12/Day12.cs
@@ -6,84 +6,36 @@ public class Day12
     public string GetSum1()
     {
         var matrix = File.ReadLines("../../../Day12/Input.txt")
-            .Select((line, row) => line.Select((c, column) => new Plant(c, row, column)))
+            .Select((line, row) => line.Select((type, column) => (row, column, type)))
             .SelectMany(x => x)
+            .GroupBy(c => c.type)
             .ToList();
 
-        var regions = new List<Region>();
-
-        foreach (var plant in matrix)
+        var regions = new List<List<(int row, int column, char type)>>();
+        foreach (var group in matrix)
         {
-            var region = regions.FirstOrDefault(r => r.ShouldContain(plant));
-            if (region == null)
+            var picked = new HashSet<(int, int, char)>();
+            foreach (var plant in group.Where(p => !picked.Contains(p)))
             {
-                region = new Region(plant.Type);
-                regions.Add(region);
-            }
-
-            region.Add(plant);
-        }
-
-        var mergedRegions = new List<Region>();
-        foreach (var region in regions)
-        {
-            var mergedRegion = mergedRegions.FirstOrDefault(r => r.AreNeighbors(region));
-            if (mergedRegion == null)
-            {
-                mergedRegions.Add(region);
-            }
-            else
-            {
-                mergedRegion.MergeWith(region);
+                picked.Add(plant);
+                regions.Add(GetNeighbours(plant, group.AsEnumerable(), picked).ToList());
             }
         }
 
-        return mergedRegions.Sum(r => r.Fence).ToString();
+        return regions.Sum(r => r.Count * r.Sum(p =>
+            4 - r.Count(p2 =>
+                Math.Abs(p2.column - p.column) +
+                Math.Abs(p.row - p2.row) == 1))).ToString();
     }
 
-    private class Plant(
-        char type, int row, int column)
+    private IEnumerable<(int row, int column, char type)> GetNeighbours(
+        (int row, int column, char type) plant,
+        IEnumerable<(int row, int column, char type)> group,
+        HashSet<(int, int, char)> picked)
     {
-        public bool IsNeighbor(Plant plant) => Math.Abs(plant.Row - Row) + Math.Abs(plant.Column - Column) == 1;
-
-        public int Row => row;
-        public int Column => column;
-        public char Type => type;
-    }
-    private class Region(char type)
-    {
-        private List<Plant> plants = new();
-
-        public bool ShouldContain(Plant plant) => plant.Type == type && plants.Any(p => p.IsNeighbor(plant));
-
-        public char Type => type;
-
-        public long Fence => plants.Select(p => 4 - GetNumberOfNeighbors(p))
-            .Sum() * plants.Count;
-
-        private int GetNumberOfNeighbors(Plant plant)
-        {
-            return plants.Count(p => p.IsNeighbor(plant));
-        }
-
-        public void Add(Plant plant)
-        {
-            plants.Add(plant);
-        }
-
-        public bool AreNeighbors(Region region)
-        {
-            if (region.Type != type)
-            {
-                return false;
-            }
-
-            return plants.Any(p => region.plants.Any(p.IsNeighbor));
-        }
-
-        public void MergeWith(Region region)
-        {
-            plants.AddRange(region.plants);
-        }
+        picked.Add(plant);
+        return group.Where(g => Math.Abs(g.column - plant.column) + Math.Abs(g.row - plant.row) == 1 && !picked.Contains(g))
+            .SelectMany(g => GetNeighbours(g, group, picked))
+            .Append(plant);
     }
 }
